8b03b5edc92a84e3e632fde6b9baf94d727b1a55
lazarcamelia <camelia.lazar18@gmail.com>
2020-05-04 16:22:09 +0300

    Avg lines added

---
+++ avgLinesInFiles.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+declare -i sumLines=0
+declare -i numberOfFiles=0
+
+if [[ -d $1 ]]; then
+  for f in `find $1 -maxdepth 1 -type f`
+  do 
+	 noLines=`cat $f | wc -l`
+	 numberOfFiles=`expr $numberOfFiles + 1`
+	 sumLines=$((noLines + sumLines))
+  done
+else
+    echo "The argument is not a directory"
+fi
+
+echo "The avg of the no of lines is `expr $sumLines / $numberOfFiles`"
